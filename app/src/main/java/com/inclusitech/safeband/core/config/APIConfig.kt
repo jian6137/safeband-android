c79d51529a3861f5d083671ef7a6fca0abb2be64
jian6137 <lbd6137@gmail.com>
2025-05-11 09:55:15 +0800

    Configure App to Fetch Server Data in Production

--- app/src/main/java/com/inclusitech/safeband/core/config/APIConfig.kt
+++ app/src/main/java/com/inclusitech/safeband/core/config/APIConfig.kt
@@ -4,7 +4,7 @@ class APIConfig {
     companion object {
         private const val PRODUCTION_DOMAIN = "https://api-safeband.zaide.online"
         private const val LOCALHOST_DOMAIN = "http://192.168.1.16:7000"
-        const val BASE_URL = "$LOCALHOST_DOMAIN/"
+        const val BASE_URL = "$PRODUCTION_DOMAIN/"
 
         const val SIGNUP_URL = "api/v1/auth/signup"
         const val LOGIN_URL = "api/v1/auth/login"
